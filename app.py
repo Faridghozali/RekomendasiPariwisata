3df3e1114f9999b76bc941afae644533ad567f14
Datamining <111824123+Faridghozali@users.noreply.github.com>
2024-05-21 21:30:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -88,87 +88,103 @@ class myCallback(tf.keras.callbacks.Callback):
 
 history = model.fit(x_train, y_train, epochs=100, validation_data=(x_val, y_val), callbacks=[myCallback()])
 
-# Sidebar 1: Recommendation System
-st.sidebar.title("Sistem")
-visualization_choice = st.sidebar.radio ("Sistem Rekomendasi Wisata")
-if visualization_choice == "Sistem Rekomendasi Wisata":                                                              
-st.title("Rekomendasi Pariwisata di Indonesia")
-user_id = st.selectbox("Pilih User ID", user['User_Id'].unique())
-place_df = place[['Place_Id', 'Place_Name', 'Category', 'Rating', 'Price']]
-place_df.columns = ['id', 'place_name', 'category', 'rating', 'price']
-place_visited_by_user = df[df.User_Id == user_id]
-place_not_visited = place_df[~place_df['id'].isin(place_visited_by_user.Place_Id.values)]['id']
-place_not_visited = list(set(place_not_visited).intersection(set(place_to_place_encoded.keys())))
-place_not_visited = [[place_to_place_encoded.get(x)] for x in place_not_visited]
-user_encoder = user_to_user_encoded.get(user_id)
-user_place_array = np.hstack(([[user_encoder]] * len(place_not_visited), place_not_visited))
+# Streamlit UI
+st.sidebar.title("Pilih Sistem atau Visualisasi")
+visualization_choice = st.sidebar.radio("Pilih opsi:", ("Sistem Rekomendasi Wisata", "Visualisasi Data"))
 
-# Predict top 7 recommendations
-ratings = model.predict(user_place_array).flatten()
-top_ratings_indices = ratings.argsort()[-7:][::-1]
-recommended_place_ids = [place_encoded_to_place.get(place_not_visited[x][0]) for x in top_ratings_indices]
+if visualization_choice == "Sistem Rekomendasi Wisata":
+    st.title("Rekomendasi Pariwisata di Indonesia")
 
-st.write(f"Daftar rekomendasi untuk: User {user_id}")
-st.write("===" * 15)
-st.write("----" * 15)
-st.write("Tempat dengan rating wisata paling tinggi dari user")
-st.write("----" * 15)
+    user['Location'] = user['Location'].apply(lambda x: x.split(',')[0])
+    user.columns = ['User_Id', 'Age', 'Location']
 
-top_place_user = place_visited_by_user.sort_values(by='Place_Ratings', ascending=False).head(5).Place_Id.values
-place_df_rows = place_df[place_df['id'].isin(top_place_user)]
-for row in place_df_rows.itertuples():
-    st.write(f"{row.place_name} : {row.category}")
+    # Pilihan input untuk Lokasi dan Umur
+    user_location = st.selectbox("Pilih Lokasi", user['Location'].unique())
+    user_age = st.slider("Pilih Umur", min_value=int(user['Age'].min()), max_value=int(user['Age'].max()), value=int(user['Age'].min()))
 
-st.write("----" * 15)
-st.write("Top 7 place recommendation")
-st.write("----" * 15)
+    # Mendapatkan User_Id yang sesuai dengan lokasi dan umur
+    user_id = user[(user['Location'] == user_location) & (user['Age'] == user_age)]['User_Id'].values
+    if len(user_id) > 0:
+        user_id = user_id[0]
+    else:
+        st.write("User dengan lokasi dan umur tersebut tidak ditemukan.")
+        st.stop()
 
-recommended_place = place_df[place_df['id'].isin(recommended_place_ids)]
-for row, i in zip(recommended_place.itertuples(), range(1, 8)):
-    st.write(f"{i}. {row.place_name}\n    {row.category}, Harga Tiket Masuk {row.price}, Rating Wisata {row.rating}\n")
+    place_df = place[['Place_Id', 'Place_Name', 'Category', 'Rating', 'Price']]
+    place_df.columns = ['id', 'place_name', 'category', 'rating', 'price']
+    place_visited_by_user = df[df.User_Id == user_id]
+    place_not_visited = place_df[~place_df['id'].isin(place_visited_by_user.Place_Id.values)]['id']
+    place_not_visited = list(set(place_not_visited).intersection(set(place_to_place_encoded.keys())))
+    place_not_visited = [[place_to_place_encoded.get(x)] for x in place_not_visited]
+    user_encoder = user_to_user_encoded.get(user_id)
+    user_place_array = np.hstack(([[user_encoder]] * len(place_not_visited), place_not_visited))
 
-st.write("===" * 15)
+    # Predict top 7 recommendations
+    ratings = model.predict(user_place_array).flatten()
+    top_ratings_indices = ratings.argsort()[-7:][::-1]
+    recommended_place_ids = [place_encoded_to_place.get(place_not_visited[x][0]) for x in top_ratings_indices]
 
-# Sidebar 2: Visualizations
-st.sidebar.title("Visualisasi Data")
-visualization_choice = st.sidebar.radio("Pilih Visualisasi:", ("Tempat Wisata Terpopuler", "Perbandingan Kategori Wisata", "Distribusi Usia User", "Distribusi Harga Tiket Masuk", "Asal Kota Pengunjung"))
+    st.write(f"Daftar rekomendasi untuk: Lokasi {user_location} dan Umur {user_age}")
+    st.write("===" * 15)
+    st.write("----" * 15)
+    st.write("Tempat dengan rating wisata paling tinggi dari user")
+    st.write("----" * 15)
 
-elif visualization_choice == "Tempat Wisata Terpopuler":
-    # Tempat wisata dengan jumlah rating terbanyak
-    top_10 = rating['Place_Id'].value_counts().reset_index()[0:10]
-    top_10 = pd.merge(top_10, place[['Place_Id', 'Place_Name']], how='left', left_on='Place_Id', right_on='Place_Id')
-    plt.figure(figsize=(8, 5))
-    sns.barplot(x='Place_Id', y='Place_Name', data=top_10)
-    plt.title('Jumlah Tempat Wisata dengan Rating Terbanyak', pad=20)
-    plt.ylabel('Jumlah Rating')
-    plt.xlabel('Nama Lokasi')
-    st.pyplot(plt)
+    top_place_user = place_visited_by_user.sort_values(by='Place_Ratings', ascending=False).head(5).Place_Id.values
+    place_df_rows = place_df[place_df['id'].isin(top_place_user)]
+    for row in place_df_rows.itertuples():
+        st.write(f"{row.place_name} : {row.category}")
 
-elif visualization_choice == "Perbandingan Kategori Wisata":
-    # Perbandingan jumlah kategori wisata
-    plt.figure(figsize=(8, 5))
-    sns.countplot(y='Category', data=place)
-    plt.title('Perbandingan Jumlah Kategori Wisata', pad=20)
-    st.pyplot(plt)
+    st.write("----" * 15)
+    st.write("Top 7 place recommendation")
+    st.write("----" * 15)
 
-elif visualization_choice == "Distribusi Usia User":
-    # Distribusi usia user
-    plt.figure(figsize=(8, 5))
-    sns.boxplot(user['Age'])
-    plt.title('Distribusi Usia User', pad=20)
-    st.pyplot(plt)
+    recommended_place = place_df[place_df['id'].isin(recommended_place_ids)]
+    for row, i in zip(recommended_place.itertuples(), range(1, 8)):
+        st.write(f"{i}. {row.place_name}\n    {row.category}, Harga Tiket Masuk {row.price}, Rating Wisata {row.rating}\n")
 
-elif visualization_choice == "Distribusi Harga Tiket Masuk":
-    # Distribusi harga masuk tempat wisata
-    plt.figure(figsize=(8, 5))
-    sns.boxplot(place['Price'])
-    plt.title('Distribusi Harga Masuk Wisata', pad=20)
-    st.pyplot(plt)
+    st.write("===" * 15)
 
-elif visualization_choice == "Asal Kota Pengunjung":
-    # Visualisasi asal kota dari user
-    askot = user['Location'].apply(lambda x: x.split(',')[0])
-    plt.figure(figsize=(8, 6))
-    sns.countplot(y=askot)
-    plt.title('Jumlah Asal Kota dari User')
-    st.pyplot(plt)
+elif visualization_choice == "Visualisasi Data":
+    st.sidebar.title("Visualisasi Data")
+    viz_choice = st.sidebar.radio("Pilih Visualisasi:", ("Tempat Wisata Terpopuler", "Perbandingan Kategori Wisata", "Distribusi Usia User", "Distribusi Harga Tiket Masuk", "Asal Kota Pengunjung"))
+
+    if viz_choice == "Tempat Wisata Terpopuler":
+        # Tempat wisata dengan jumlah rating terbanyak
+        top_10 = rating['Place_Id'].value_counts().reset_index()[0:10]
+        top_10 = pd.merge(top_10, place[['Place_Id', 'Place_Name']], how='left', left_on='Place_Id', right_on='Place_Id')
+        plt.figure(figsize=(8, 5))
+        sns.barplot(x='Place_Id', y='Place_Name', data=top_10)
+        plt.title('Jumlah Tempat Wisata dengan Rating Terbanyak', pad=20)
+        plt.ylabel('Jumlah Rating')
+        plt.xlabel('Nama Lokasi')
+        st.pyplot(plt)
+
+    elif viz_choice == "Perbandingan Kategori Wisata":
+        # Perbandingan jumlah kategori wisata
+        plt.figure(figsize=(8, 5))
+        sns.countplot(y='Category', data=place)
+        plt.title('Perbandingan Jumlah Kategori Wisata', pad=20)
+        st.pyplot(plt)
+
+    elif viz_choice == "Distribusi Usia User":
+        # Distribusi usia user
+        plt.figure(figsize=(8, 5))
+        sns.boxplot(user['Age'])
+        plt.title('Distribusi Usia User', pad=20)
+        st.pyplot(plt)
+
+    elif viz_choice == "Distribusi Harga Tiket Masuk":
+        # Distribusi harga masuk tempat wisata
+        plt.figure(figsize=(8, 5))
+        sns.boxplot(place['Price'])
+        plt.title('Distribusi Harga Masuk Wisata', pad=20)
+        st.pyplot(plt)
+
+    elif viz_choice == "Asal Kota Pengunjung":
+        # Visualisasi asal kota dari user
+        askot = user['Location'].apply(lambda x: x.split(',')[0])
+        plt.figure(figsize=(8, 6))
+        sns.countplot(y=askot)
+        plt.title('Jumlah Asal Kota dari User')
+        st.pyplot(plt)
