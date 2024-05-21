c74d29d1f2d3a24b3f1c82aec1c9153c7643486f
Datamining <111824123+Faridghozali@users.noreply.github.com>
2024-05-21 18:33:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from zipfile import ZipFile
-from pathlib import Path
-import seaborn as sns
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
@@ -58,31 +55,38 @@ def main():
     top_10 = pd.merge(top_10, place[['Place_Id','Place_Name']], how='left', left_on='Place_Id', right_on='Place_Id')
     
     plt.figure(figsize=(8,5))
-    sns.barplot(x='Place_Id', y='Place_Name', data=top_10)
+    plt.bar(top_10['Place_Id'], top_10['Place_Name'])
     plt.title('Jumlah Tempat Wisata dengan Rating Terbanyak', pad=20)
     plt.ylabel('Jumlah Rating')
     plt.xlabel('Nama Lokasi')
     st.pyplot(plt.gcf())
     
-    sns.countplot(y='Category', data=place)
+    plt.figure(figsize=(6,4))
+    place['Category'].value_counts().plot(kind='bar')
     plt.title('Perbandingan Jumlah Kategori Wisata ', pad=20)
+    plt.xlabel('Kategori')
+    plt.ylabel('Jumlah')
     st.pyplot(plt.gcf())
     
     plt.figure(figsize=(5,3))
-    sns.boxplot(user['Age'])
+    plt.boxplot(user['Age'])
     plt.title('Distribusi Usia User', pad=20)
+    plt.xlabel('Usia')
     st.pyplot(plt.gcf())
     
-    plt.figure(figsize=(7,3))
-    sns.boxplot(place['Price'])
+    plt.figure(figsize=(5,3))
+    plt.boxplot(place['Price'])
     plt.title('Distribusi Harga Masuk Wisata ', pad=20)
+    plt.xlabel('Harga')
     st.pyplot(plt.gcf())
     
     askot = user['Location'].apply(lambda x : x.split(',')[0])
     
     plt.figure(figsize=(8,6))
-    sns.countplot(y=askot)
+    askot.value_counts().plot(kind='barh')
     plt.title('Jumlah Asal Kota dari User')
+    plt.xlabel('Jumlah User')
+    plt.ylabel('Kota')
     st.pyplot(plt.gcf())
     
     # Encoding data
@@ -144,66 +148,4 @@ def main():
     
     model.compile(
         loss=tf.keras.losses.BinaryCrossentropy(),
-        optimizer=keras.optimizers.Adam(learning_rate=0.0004),
-        metrics=[tf.keras.metrics.RootMeanSquaredError()]
-    )
-    
-    class myCallback(tf.keras.callbacks.Callback):
-        def on_epoch_end(self, epoch, logs={}):
-            if(logs.get('val_root_mean_squared_error') < 0.25):
-                st.write('Lapor! Metriks validasi sudah sesuai harapan')
-                self.model.stop_training = True
-    
-    history = model.fit(
-        x=x_train,
-        y=y_train,
-        epochs=100,
-        validation_data=(x_val, y_val),
-        callbacks=[myCallback()]
-    )
-    
-    st.subheader("Plot Loss dan Validation")
-    plt.plot(history.history['root_mean_squared_error'])
-    plt.plot(history.history['val_root_mean_squared_error'])
-    plt.title('model_metrics')
-    plt.ylabel('root_mean_squared_error')
-    plt.xlabel('epoch')
-    plt.ylim(ymin=0, ymax=0.4)
-    plt.legend(['train', 'test'], loc='center left')
-    st.pyplot(plt.gcf())
-    
-    place_df = place[['Place_Id','Place_Name','Category','Rating','Price']]
-    place_df.columns = ['id','place_name','category','rating','price']
-    df = rating.copy()
-    
-    user_id = st.selectbox('Pilih User ID', df.User_Id.unique())
-    place_visited_by_user = df[df.User_Id == user_id]
-    
-    place_not_visited = place_df[~place_df['id'].isin(place_visited_by_user.Place_Id.values)]['id']
-    place_not_visited = list(set(place_not_visited).intersection(set(place_to_place_encoded.keys())))
-    place_not_visited = [[place_to_place_encoded.get(x)] for x in place_not_visited]
-    user_encoder = user_to_user_encoded.get(user_id)
-    user_place_array = np.hstack(([[user_encoder]] * len(place_not_visited), place_not_visited))
-    
-    ratings = model.predict(user_place_array).flatten()
-    top_ratings_indices = ratings.argsort()[-7:][::-1]
-    recommended_place_ids = [place_encoded_to_place.get(place_not_visited[x][0]) for x in top_ratings_indices]
-    
-    st.subheader('Daftar Rekomendasi')
-    st.write(f"Rekomendasi untuk User {user_id}")
-    
-    top_place_user = place_visited_by_user.sort_values(by='Place_Ratings', ascending=False).head(5).Place_Id.values
-    place_df_rows = place_df[place_df['id'].isin(top_place_user)]
-    
-    st.write('Tempat dengan rating wisata paling tinggi dari user:')
-    for row in place_df_rows.itertuples():
-        st.write(f"{row.place_name} : {row.category}")
-    
-    st.write('Top 7 rekomendasi tempat wisata:')
-    recommended_place = place_df[place_df['id'].isin(recommended_place_ids)]
-    for row, i in zip(recommended_place.itertuples(), range(1, 8)):
-        st.write(f"{i}. {row.place_name} \n    {row.category}, Harga Tiket Masuk {row.price}, Rating Wisata {row.rating}")
-
-if __name__ == "__main__":
-    main()
-
+        optimizer=keras.optimizers
