3ad49c8977564a0a9d6af40acb11aeaca42ce0c9
Datamining <111824123+Faridghozali@users.noreply.github.com>
2024-06-07 22:26:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,26 +10,22 @@ info_tourism = pd.read_csv("tourism_with_id.csv")
 # Tab pertama: Filter Tempat Wisata
 def filter_places():
     st.title('Filter Tempat Wisata')
-    min_price = info_tourism['Price'].min()
-    max_price = info_tourism['Price'].max()
-    categories = st.sidebar.selectbox('Category wisata?', info_tourism['Category'].unique())
-    cities = st.sidebar.selectbox('Lokasi?', info_tourism['City'].unique())
+    categories = st.selectbox('Category wisata?', info_tourism['Category'].unique())
+    cities = st.selectbox('Lokasi?', info_tourism['City'].unique())
     selected_price_range = st.sidebar.slider('Range Harga?', min_value=min_price, max_value=max_price, value=(min_price, max_price))
 
     min_price, max_price = selected_price_range
 
     # Filter data berdasarkan input pengguna
     filtered_data = info_tourism[(info_tourism['Category'] == categories) &
-                                 (info_tourism['City'] == cities) &
-                                 (info_tourism['Price'] >= min_price) &
-                                 (info_tourism['Price'] <= max_price)]
+                                 (info_tourism['City'] == cities)]
 
     # Tampilkan hasil filter
     st.header('Tempat Wisata yang Sesuai dengan Preferensi Kamu')
     if len(filtered_data) == 0:
         st.write('Maaf, tidak ada tempat wisata yang sesuai dengan preferensi Kamu.')
     else:
-        st.write(filtered_data[['Place_Name', 'Description', 'Category', 'City', 'Price', 'Rating']])
+        st.write(filtered_data[['Place_Name', 'Category', 'City', 'Price', 'Rating']])
 # Main App
 st.title("Sistem Rekomendasi Tempat Wisata")
 
