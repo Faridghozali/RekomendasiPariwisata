c7791f23ba8374d7ad651ae03ee642477a410941
Datamining <111824123+Faridghozali@users.noreply.github.com>
2024-06-07 21:47:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,129 +1,76 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 import string
 
 # Load data
-info_tourism = pd.read_csv("https://raw.githubusercontent.com/khikisb/SistemRekomendasiWisata/main/tourism_with_id.csv")
+info_tourism = pd.read_csv("tourism_with_id.csv")
 
-# Load dataset
-@st.cache
-def load_data():
-    rating = pd.read_csv('tourism_rating.csv')
-    place = pd.read_csv('tourism_with_id.csv')
-    user = pd.read_csv('user.csv')
-    return rating, place, user
+# Tab pertama: Filter Tempat Wisata
+def filter_places():
+    st.sidebar.title('Filter Tempat Wisata')
+    min_price = info_tourism['Price'].min()
+    max_price = info_tourism['Price'].max()
+    categories = st.sidebar.selectbox('Category wisata?', info_tourism['Category'].unique())
+    cities = st.sidebar.selectbox('Lokasi?', info_tourism['City'].unique())
+    selected_price_range = st.sidebar.slider('Range Harga?', min_value=min_price, max_value=max_price, value=(min_price, max_price))
 
-rating, place, user = load_data()
+    min_price, max_price = selected_price_range
 
-# Drop unnecessary columns
-place = place.drop(['Unnamed: 11', 'Unnamed: 12'], axis=1)
-place = place.drop('Time_Minutes', axis=1)
+    # Filter data berdasarkan input pengguna
+    filtered_data = info_tourism[(info_tourism['Category'] == categories) &
+                                 (info_tourism['City'] == cities) &
+                                 (info_tourism['Price'] >= min_price) &
+                                 (info_tourism['Price'] <= max_price)]
 
-# Filter ratings for places 
-rating = pd.merge(rating, place[['Place_Id']], how='right', on='Place_Id')
+    # Tampilkan hasil filter
+    st.header('Tempat Wisata yang Sesuai dengan Preferensi Kamu')
+    if len(filtered_data) == 0:
+        st.write('Maaf, tidak ada tempat wisata yang sesuai dengan preferensi Kamu.')
+    else:
+        st.write(filtered_data[['Place_Name', 'Description', 'Category', 'City', 'Price', 'Rating']])
 
-# Filter users who have visited places
-user = pd.merge(user, rating[['User_Id']], how='right', on='User_Id').drop_duplicates().sort_values('User_Id')
+# Tab kedua: Rekomendasi berdasarkan deskripsi
+def recommend_by_description():
+    user_input = st.text_area("Ceritakan kamu mau pergi kemana? dengan siapa?dan ingin melakukan apa?")
+    st.write('Contoh : saya ingin pergi dengan keluarga dan ingin melihat lukisan lukisan yang indah')
+    st.write('Contoh : saya ingin pergi ke pantai yang masih jarang orang tahu')
+    if user_input:
+        # Pra-pemrosesan teks pada input pengguna
+        stop_factory = StopWordRemoverFactory()
+        stop_words = stop_factory.get_stop_words()
+        stop_words.extend(string.punctuation)
 
-# Encoding function
-def dict_encoder(col, data):
-    unique_val = data[col].unique().tolist()
-    val_to_val_encoded = {x: i for i, x in enumerate(unique_val)}
-    val_encoded_to_val = {i: x for i, x in enumerate(unique_val)}
-    return val_to_val_encoded, val_encoded_to_val
+        tfidf = TfidfVectorizer(stop_words=stop_words)
+        info_tourism['Description'] = info_tourism['Description'].fillna('')
+        tfidf_matrix = tfidf.fit_transform(info_tourism['Description'])
 
-# Encoding User_Id and Place_Id
-user_to_user_encoded, user_encoded_to_user = dict_encoder('User_Id', rating)
-place_to_place_encoded, place_encoded_to_place = dict_encoder('Place_Id', rating)
+        user_tfidf = tfidf.transform([user_input])
 
-rating['user'] = rating['User_Id'].map(user_to_user_encoded)
-rating['place'] = rating['Place_Id'].map(place_to_place_encoded)
+        # Hitung cosine similarity antara input pengguna dan deskripsi tempat wisata
+        similarity_scores = cosine_similarity(user_tfidf, tfidf_matrix)
 
-num_users, num_place = len(user_to_user_encoded), len(place_to_place_encoded)
-rating['Place_Ratings'] = rating['Place_Ratings'].values.astype(np.float32)
-min_rating, max_rating = min(rating['Place_Ratings']), max(rating['Place_Ratings'])
+        # Dapatkan indeks tempat wisata yang direkomendasikan berdasarkan similarity scores
+        recommended_indices = similarity_scores.argsort()[0][::-1][:5]
 
-# Shuffle the dataset
-df = rating.sample(frac=1, random_state=42)
+        # Tampilkan tempat wisata yang direkomendasikan
+        recommended_places = info_tourism.iloc[recommended_indices][['Place_Name', 'Description', 'Category', 'City', 'Price', 'Rating']]
+        st.write("Tempat wisata yang direkomendasikan berdasarkan deskripsi Kamu:")
+        st.write(recommended_places)
+    else:
+        st.write("Hindari menggunakan nama kota, Karena kami akan merekomendasikan tempat yang paling cocok dengan Kamu di Seluruh Indonesia")
 
-# Prepare training and validation data
-x = df[['user', 'place']].values
-y = df['Place_Ratings'].apply(lambda x: (x - min_rating) / (max_rating - min_rating)).values
-train_indices = int(0.8 * df.shape[0])
-x_train, x_val, y_train, y_val = x[:train_indices], x[train_indices:], y[:train_indices], y[train_indices:]
+# Main App
+st.title("Sistem Rekomendasi Tempat Wisata")
 
-# Define the RecommenderNet model
-class RecommenderNet(tf.keras.Model):
-    def __init__(self, num_users, num_places, embedding_size, **kwargs):
-        super(RecommenderNet, self).__init__(**kwargs)
-        self.num_users = num_users
-        self.num_places = num_places
-        self.embedding_size = embedding_size
-        self.user_embedding = layers.Embedding(num_users, embedding_size, embeddings_initializer='he_normal', embeddings_regularizer=tf.keras.regularizers.l2(1e-6))
-        self.user_bias = layers.Embedding(num_users, 1)
-        self.places_embedding = layers.Embedding(num_places, embedding_size, embeddings_initializer='he_normal', embeddings_regularizer=tf.keras.regularizers.l2(1e-6))
-        self.places_bias = layers.Embedding(num_places, 1)
+# Pilihan tab
+tabs = ["Filter Tempat Wisata", "Rekomendasi berdasarkan Deskripsi"]
+choice = st.sidebar.radio("Navigasi", tabs)
 
-    def call(self, inputs):
-        user_vector = self.user_embedding(inputs[:, 0])
-        user_bias = self.user_bias(inputs[:, 0])
-        places_vector = self.places_embedding(inputs[:, 1])
-        places_bias = self.places_bias(inputs[:, 1])
-        dot_user_places = tf.tensordot(user_vector, places_vector, 2)
-        x = dot_user_places + user_bias + places_bias
-        return tf.nn.sigmoid(x)
-
-model = RecommenderNet(num_users, num_place, 50)
-
-# Compile the model
-model.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=tf.keras.optimizers.Adam(learning_rate=0.0004), metrics=[tf.keras.metrics.RootMeanSquaredError()])
-
-# Train the model
-class myCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs=None):
-        if logs.get('val_root_mean_squared_error') < 0.25:
-            print('Lapor! Metriks validasi sudah sesuai harapan')
-            self.model.stop_training = True
-
-history = model.fit(x_train, y_train, epochs=100, validation_data=(x_val, y_val), callbacks=[myCallback()])
-
-# Streamlit UI
-st.sidebar.title("Pilih Sistem atau Visualisasi")
-visualization_choice = st.sidebar.radio("Pilih opsi:", ("Sistem Rekomendasi Wisata", "Visualisasi Data"))
-
-if visualization_choice == "Sistem Rekomendasi Wisata":
-    st.title("Rekomendasi Pariwisata di Indonesia")
-    
-    user_id = st.selectbox("Pilih User ID", user['User_Id'].unique())
-    
-    place_df = place[['Place_Id', 'Place_Name', 'Category', 'Rating', 'Price']]
-    place_df.columns = ['id', 'place_name', 'category', 'rating', 'price']
-    
-    place_visited_by_user = df[df.User_Id == user_id]
-    place_not_visited = place_df[~place_df['id'].isin(place_visited_by_user.Place_Id.values)]['id']
-    place_not_visited = list(set(place_not_visited).intersection(set(place_to_place_encoded.keys())))
-    place_not_visited = [[place_to_place_encoded.get(x)] for x in place_not_visited]
-    
-    user_encoder = user_to_user_encoded.get(user_id)
-    user_place_array = np.hstack(([[user_encoder]] * len(place_not_visited), place_not_visited))
-    
-    # Predict top 7 recommendations
-    ratings = model.predict(user_place_array).flatten()
-    top_ratings_indices = ratings.argsort()[-7:][::-1]
-    recommended_place_ids = [place_encoded_to_place.get(place_not_visited[x][0]) for x in top_ratings_indices]
-    
-    st.write(f"Daftar rekomendasi untuk: User {user_id}")
-    st.write("===" * 15)
-    st.write("----" * 15)
-    st.write("Tempat dengan rating wisata paling tinggi dari user")
-    st.write("----" * 15)
-    
-    top_place_user = place_visited
+# Tampilkan tab yang dipilih
+if choice == "Filter Tempat Wisata":
+    filter_places()
+elif choice == "Rekomendasi berdasarkan Deskripsi":
+    recommend_by_description()
