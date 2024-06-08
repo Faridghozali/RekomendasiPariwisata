0aa1b8c1a0140176aae167b92cd846869f2c480a
Datamining <111824123+Faridghozali@users.noreply.github.com>
2024-06-08 10:33:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ info_tourism = pd.read_csv("tourism_with_id.csv")
 
 # Tab pertama: Filter Tempat Wisata
 def filter_places():
-    st.title('Filter Tempat Wisata')
-    
     # Input user for name and age
     name = st.text_input('Masukkan nama kamu:')
     age = st.number_input('Masukkan umur kamu:', min_value=10, max_value=100)
     
-    categories = st.selectbox('Category wisata?', info_tourism['Category'].unique())
-    cities = st.selectbox('Lokasi?', info_tourism['City'].unique())
+    categories = st.selectbox('Masukkan kategori wisata', info_tourism['Category'].unique())
+    cities = st.selectbox('Masukan Lokasi kamu', info_tourism['City'].unique())
 
     # Tampilkan hasil filter hanya jika semua inputan sudah terisi
     if name and age and categories and cities:
@@ -22,24 +20,26 @@ def filter_places():
         filtered_data = info_tourism[(info_tourism['Category'] == categories) &
                                      (info_tourism['City'] == cities)]
 
-        st.header(f'Daftar rekomendasi wisata untuk {name} yang berumur {age} tahun')
+        st.header(f'Daftar rekomendasi wisata untuk {name} yang berumur {age} tahun :')
 
         if len(filtered_data) == 0:
-            st.write('Maaf, tidak ada tempat wisata yang sesuai dengan preferensi Kamu.')
+            st.write('Mohon maaf, tidak ada rekomendasi tempat wisata yang sesuai dengan preferensi Kamu saat ini.')
         else:
             st.write(filtered_data[['Place_Name', 'Category', 'City', 'Price', 'Rating']])
     else:
         st.write('Silakan lengkapi semua input untuk melihat rekomendasi tempat wisata.')
 
 # Main App
-st.title("Sistem Rekomendasi Tempat Wisata")
+st.title("Rekomendasi Tempat Wisata di Indonesia")
 
 # Pilihan tab
-tabs = ["Filter Tempat Wisata"]
-choice = st.sidebar.radio("Navigasi", tabs)
+tabs = ["Sistem Rekomendasi Wisata"]
+choice = st.sidebar.radio("Pilihan Menu", tabs)
 
 # Tampilkan tab yang dipilih
-if choice == "Filter Tempat Wisata":
+if choice == "Sistem Rekomendasi Wisata":
     filter_places()
-elif choice == "Rekomendasi berdasarkan Deskripsi":
+if choice == "Sistem Rekomendasi Wisata dari User sebelumnya":
+    filter_places()
+elif choice == "Visualisasi Data":
     recommend_by_description()
