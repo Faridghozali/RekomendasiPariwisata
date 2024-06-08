426aa4713c80841882219a14ad9b0d1faac22aff
Datamining <111824123+Faridghozali@users.noreply.github.com>
2024-06-08 10:23:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,25 @@ def filter_places():
     
     # Input user for name and age
     name = st.text_input('Masukkan nama kamu:')
-    age = st.number_input('Masukkan umur kamu:', min_value=1, max_value=100)
+    age = st.number_input('Masukkan umur kamu:', min_value=10, max_value=100)
     
     categories = st.selectbox('Category wisata?', info_tourism['Category'].unique())
     cities = st.selectbox('Lokasi?', info_tourism['City'].unique())
 
-    # Filter data berdasarkan input pengguna
-    filtered_data = info_tourism[(info_tourism['Category'] == categories) &
-                                 (info_tourism['City'] == cities)]
+    # Tampilkan hasil filter hanya jika semua inputan sudah terisi
+    if name and age and categories and cities:
+        # Filter data berdasarkan input pengguna
+        filtered_data = info_tourism[(info_tourism['Category'] == categories) &
+                                     (info_tourism['City'] == cities)]
 
-    # Tampilkan hasil filter
-    if name and age:
         st.header(f'Daftar rekomendasi wisata untuk {name} yang berumur {age} tahun')
-    else:
-        st.header('Daftar rekomendasi wisata')
 
-    if len(filtered_data) == 0:
-        st.write('Maaf, tidak ada tempat wisata yang sesuai dengan preferensi Kamu.')
+        if len(filtered_data) == 0:
+            st.write('Maaf, tidak ada tempat wisata yang sesuai dengan preferensi Kamu.')
+        else:
+            st.write(filtered_data[['Place_Name', 'Category', 'City', 'Price', 'Rating']])
     else:
-        st.write(filtered_data[['Place_Name', 'Category', 'City', 'Price', 'Rating']])
+        st.write('Silakan lengkapi semua input untuk melihat rekomendasi tempat wisata.')
 
 # Main App
 st.title("Sistem Rekomendasi Tempat Wisata")
